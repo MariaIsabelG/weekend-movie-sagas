e68b55fec45489a4d7eb1ad3f183c2cee0fa4612
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-07-21 15:55:58 -0500

    added useHistory to send user to the details page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import { useDispatch, useSelector, useHistory } from 'react-redux';
+import './MovieList.css';
+
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    const history = useHistory();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    const movieDetails = () => {
+        history.push('/details')
+    }
 
     return (
         <main>
@@ -19,7 +25,7 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img onClick={movieDetails} src={movie.poster} alt={movie.title}/>
                         </div>
                     );
                 })}
