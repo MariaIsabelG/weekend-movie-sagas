92912cae79472f37550268afffd19925d071f8ee
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-07-23 10:07:01 -0500

    completed the react part for the MovieDetails

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,19 +1,34 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 
 
 
 function MovieDetails (){
 
-    const details = useSelector( store => store.genres)
+    const details = useSelector( (store) => store.genres)
+
+    useEffect(() => {
+        console.log ( 'These are the details from the movie:', details)
+    }, []);
+    
 
     return (
-        <p>Movie Details</p>
+        <div>
+        <h1>Movie Details</h1>
 
+        <img src={details[0].poster}/>
+        <h3>Title:</h3> <p>{details[0].title}</p>
+        <h3>Description:</h3> <p>{details[0].description}</p>
+        <h3>Genres:</h3>
+        {details.map(( detail =>{
+            return ( <p>{detail.name}</p>)
+        })
+        )}
 
-
-
+        </div>
     )
-}
+};
+
 
 export default MovieDetails;
